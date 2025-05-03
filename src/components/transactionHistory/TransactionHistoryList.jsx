e8c84b250450ddbf27490f6d066f076335c97002
Oluwadunsin <oluwadunsin2016@gmail.com>
2025-05-03 17:24:40 +0100

    Updated

--- src/components/transactionHistory/TransactionHistoryList.jsx
+++ src/components/transactionHistory/TransactionHistoryList.jsx
@@ -85,7 +85,7 @@ const TransactionHistoryList = ({transactions,goNext}) => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             <h3 className="mt-2 text-sm font-medium text-gray-900">No transactions</h3>
-            <p className="mt-1 text-sm text-gray-500">This user has no transaction history.</p>
+            <p className="mt-1 text-sm text-gray-500">You have no transaction history.</p>
           </div>
         )}
       </div>
